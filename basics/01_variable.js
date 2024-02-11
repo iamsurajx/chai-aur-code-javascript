6eabb559477766f83c15a1de4a738bffe5134637
Suraj Choudhary <66207975+iamsurajx@users.noreply.github.com>
2024-02-11 05:33:51 +0000

    variable and datatype

---
+++ basics/01_variable.js
@@ -0,0 +1,7 @@
+const accountId = 122123;
+let accountEmail = "surajemail@gmai.com";
+let accountPassword = "121212";
+// var accountCity= "Delhi" //var is global scope preafer not to use it
+
+console.log(accountEmail,accountId,accountPassword)
+console.table([accountEmail, accountPassword, accountId]);
---
+++ basics/02_datatypes.js
@@ -0,0 +1,5 @@
+"use strict" // iska matlab treat all code as new version, agra older version wala code likhe to error throw karega.
+
+//alert("hello....") //browser par work karega not in node js
+
+console.log(3 + 3)
