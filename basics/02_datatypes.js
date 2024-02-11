eec09b12e000655dc8ecaa0fc85cf851ea8fd943
Suraj Choudhary <66207975+iamsurajx@users.noreply.github.com>
2024-02-11 05:44:14 +0000

    02_datatypes completed

--- basics/02_datatypes.js
+++ basics/02_datatypes.js
@@ -3,3 +3,15 @@
 //alert("hello....") //browser par work karega not in node js
 
 console.log(3 + 3)
+
+//number => 2 to power 53
+//bigint =>
+//string =>""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique 
+//object =>
+
+console.log(typeof undefined); //undefined
+conaole.log(typeof null); //object
